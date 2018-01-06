d62d4790ef1e60c3be94f621da9ecca9dcd3a7c6
Emily Reller <relleremily@hotmail.com>
2018-01-06 12:02:30 -0600

    MenuController finished

--- src/CheeseMVC/Migrations/20180106162109_MenuController2.Designer.cs
+++ src/CheeseMVC/Migrations/20180106162109_MenuController2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180106162109_MenuController2")]
+    partial class MenuController2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
